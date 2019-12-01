ac7fe5e5b063ea82bcec93e0895e5f49c43adf90
WILLIAM X F WANG <1016795105@qq.com>
2019-12-01 15:06:28 +0800

    the makefile is totally understand

--- app/dep/ngx_conf.d
+++
@@ -1,2 +0,0 @@
-/mnt/hgfs/LinuxSharedFolder/william_nginx/app/link_obj/ngx_conf.o: ngx_conf.c \
- /mnt/hgfs/LinuxSharedFolder/william_nginx/_include/ngx_func.h
--- app/dep/ngx_signal.d
+++
@@ -1,2 +0,0 @@
-/mnt/hgfs/LinuxSharedFolder/william_nginx/app/link_obj/ngx_signal.o: ngx_signal.c \
- /mnt/hgfs/LinuxSharedFolder/william_nginx/_include/ngx_signal.h
